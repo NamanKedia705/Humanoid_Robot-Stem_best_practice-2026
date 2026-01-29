00f1c975be7cf2100e07107f741ae0a4da5343a2
Naman Kedia <180081538+NamanKedia705@users.noreply.github.com>
2026-01-29 15:53:54 +0530

    Modify speech rate and set volume in speech_output
    Adjusted speech rate and set volume level.

--- speech_output.py
+++ speech_output.py
@@ -2,6 +2,8 @@ import pyttsx3
 
 engine = pyttsx3.init()
 engine.setProperty("rate", 150)
+engine.setProperty("rate", 145)
+engine.setProperty("volume", 1.0)
 
 def speak(text):
     print("SPEAK:", text)
